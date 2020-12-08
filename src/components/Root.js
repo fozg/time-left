63713de817fe0ba1fac39b4976b14921215aa8e4
Phong <phongdaohong@gmail.com>
2020-12-08 18:42:32 +0700

    Update Root.js

--- src/components/Root.js
+++ src/components/Root.js
@@ -3,13 +3,14 @@ import CalendarRow from './CalendarRow';
 import Line from './Line';
 
 const tasks = [
-    { title: "Testing number 1", end: new Date("10/23/2020"), backgroundColor: "#009688" },
-    { title: "Testing number 2", end: new Date("10/29/2020"), backgroundColor: "#3f51b5" },
-    { title: "Hello world", end: new Date("10/23/2020"), backgroundColor: "#2196f3" },
-    { title: "Hello world", end: new Date("11/01/2020"), backgroundColor: "#4caf50" },
-    { title: "Hello world", end: new Date("11/06/2020"), backgroundColor: "#ff5722" },
-    { title: "Hello world", end: new Date("11/10/2020"), backgroundColor: "#ff9800" },
+    { title: "Testing number 1", end: Date.now() + 1000 * 60 * 60 * 24 * 5, backgroundColor: "#009688" },
+    { title: "Testing number 2", end: Date.now() + 1000 * 60 * 60 * 24 * 6, backgroundColor: "#3f51b5" },
+    { title: "Hello world", end: Date.now() + 1000 * 60 * 60 * 24 * 3, backgroundColor: "#2196f3" },
+    { title: "Hello world", end: Date.now() + 1000 * 60 * 60 * 24 * 10, backgroundColor: "#4caf50" },
+    { title: "Hello world", end: Date.now() + 1000 * 60 * 60 * 24 * 8, backgroundColor: "#ff5722" },
+    { title: "Hello world", end: Date.now() + 1000 * 60 * 60 * 24 * 1, backgroundColor: "#ff9800" },
 ]
+
 
 
 const TOTAL = 2678400000;
